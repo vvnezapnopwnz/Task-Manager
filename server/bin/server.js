b4ea336632ac90ddac358a827332f429af760d92
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2022-01-22 18:45:11 +0600

    adding fastify template

--- server/bin/server.js
+++ server/bin/server.js
@@ -5,12 +5,6 @@ import getApp from '../index.js';
 const port = process.env.PORT || 5000;
 const address = '0.0.0.0';
 
-getApp().listen(port, address, (err) => {
-  if (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-    process.exit(1);
-  }
-  // eslint-disable-next-line no-console
+getApp().listen(port, address, () => {
   console.log(`Server is running on port: ${port}`);
 });
