2f8d535037cca0898f78d61b20a2d6356505dd54
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2022-04-30 16:00:05 +0600

    task model ref 1

--- server/models/Task.cjs
+++ server/models/Task.cjs
@@ -18,8 +18,8 @@ module.exports = class Task extends unique(BaseModel) {
         id: { type: 'integer' },
         name: { type: 'string', minLength: 1 },
         description: { type: 'string' },
-        statusId: { type: 'integer' },
-        creatorId: { type: 'integer' },
+        statusId: { type: 'integer', minimum: 1 },
+        creatorId: { type: 'integer', minimum: 1 },
         executorId: { type: ['integer', 'null'] },
       },
     };
