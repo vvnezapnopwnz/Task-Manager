37f49702e7aca46cb271bd1b715b94f4f555b5e0
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2022-05-02 19:58:59 +0600

    eslint error cheking ref 1

--- server/migrations/20220417080503_tasks.js
+++ server/migrations/20220417080503_tasks.js
@@ -1,16 +1,17 @@
 // @ts-check
 
 export const up = (knex) => (
-    knex.schema.createTable('tasks', (table) => {
-      table.increments('id').primary();
-      table.string('name');
-      table.string('description');
-      table.integer('status_id').index().references('id').inTable('task_statuses');
-      table.integer('creator_id').index().references('id').inTable('users');
-      table.integer('executor_id').index().unsigned().nullable().references('id').inTable('users');
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-      })
-    );
-    
-  export const down = (knex) => knex.schema.dropTable('tasks');
+  knex.schema.createTable('tasks', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.string('description');
+    table.integer('status_id').index().references('id').inTable('task_statuses');
+    table.integer('creator_id').index().references('id').inTable('users');
+    table.integer('executor_id').index().unsigned().nullable()
+      .references('id').inTable('users');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+  );
+
+export const down = (knex) => knex.schema.dropTable('tasks');
